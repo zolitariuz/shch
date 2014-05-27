11127abd684fabfcc3bb78e36b2d02b5bfa231ce
maichomper <maichomper@gmail.com>
2014-05-26 21:40:17 -0500

    pagination links

--- themes/shch/category-noticias.php
+++ themes/shch/category-noticias.php
@@ -1,42 +1,5 @@
 <?php get_header(); ?>
 
-	<style>
-		ul.page-numbers {
-	margin: 20px 0 10px;
-	width: 100%;
-	padding: 0;
-	font-size: 12px;
-	line-height: normal;
-	clear: both;
-	float: left;
-	}
-    
-    ul.page-numbers li {
-    	float: left;
-    	}
-
-ul.page-numbers a,
-ul.page-numbers span {
-	border-radius: 3px;
-	-moz-border-radius: 3px;
-	-webkit-border-radius: 3px;
-	background: -webkit-gradient(linear, left top, left bottom, from(#E4E3E3), to(#FFFFFF));
-	background: -moz-linear-gradient(top,  #E4E3E3,  #FFFFFF);
-	filter:  progid:DXImageTransform.Microsoft.gradient(startColorstr='#E4E3E3', endColorstr='#FFFFFF');
-	padding: 3px 4px 2px 4px; 
-	margin: 2px;
-	text-decoration: none;
-	border: 1px solid #ccc;
-	color: #666;
-	}
-
-ul.page-numbers a:hover,
-ul.page-numbers span.current {	
-	border: 1px solid #666;
-	color: #444;
-	}
-	</style>
-
 	<div class="hero full">
 
 		<?php 
@@ -71,16 +34,7 @@ ul.page-numbers span.current {
 
 			<?php
 			global $wp_query;
-
-			$type = 'noticias';
-			$noticiasArgs = array(
-				'category_name'		=> $type,
-				'posts_per_page'	=> 3
-			);
-
-			$noticiasQuery = new WP_Query($noticiasArgs);
-			if($noticiasQuery -> have_posts()) : while($noticiasQuery -> have_posts()) : 
-					$noticiasQuery ->the_post(); 
+			if(have_posts()) : while(have_posts()) : the_post(); 
 
 					if ( has_post_thumbnail() ) {
 						$imgUrl = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ) ;
@@ -92,10 +46,11 @@ ul.page-numbers span.current {
 						<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
 						<p><?php the_excerpt(); ?></p>
 					</div><!-- post -->
+	
+			<?php endwhile; endif; wp_reset_query(); ?>
 
-				<?php
-				
-
+			<div class="columna full text-center">
+			<?php
 				$big = 999999999; // need an unlikely integer
 
 				echo paginate_links( array(
@@ -104,13 +59,9 @@ ul.page-numbers span.current {
 					'current' => max( 1, get_query_var('paged') ),
 					'total' => $wp_query->max_num_pages
 				) );
-				?>
-	
 
-				<?php endwhile; endif; wp_reset_query(); 
-				
 			?>
-
+			</div>
 		</section>
 
 	</div><!-- width -->
